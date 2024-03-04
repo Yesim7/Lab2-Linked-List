1d6d30034af2c1d8cdfe549ec62786f54a9a0c35
yesimaygul <yesimaygul7@gmail.com>
2024-03-04 10:53:20 +0300

    Lab2 Bagli Liste

---
+++ BagliListeler/BagliListeler/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BagliListeler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LinkedListADT ll = new LinkedListADT();
+
+            ll.InsertFirst(6);
+            ll.InsertFirst(5);
+            ll.InsertFirst(4);
+            ll.InsertFirst(3);
+            ll.InsertFirst(2);
+            ll.InsertFirst(1);
+            ll.InsertLast(7);
+            ll.InsertLast(8);
+            ll.InsertPos(2, 10);
+            MessageBox.Show(ll.DisplayElements());
+
+            ll.DeleteFirst();
+            MessageBox.Show(ll.DisplayElements());
+
+            ll.DeleteLast();
+            MessageBox.Show(ll.DisplayElements());
+
+            ll.DeleteLast();
+            MessageBox.Show(ll.DisplayElements());
+
+            ll.DeletePos(2);
+            MessageBox.Show(ll.DisplayElements());
+
+            Node testNode = ll.GetElement(4);
+            MessageBox.Show(testNode.Data.ToString());
+
+
+            //c# içerisinde Tanımlı olarak gelen LinkedListSınıfı
+
+            /*
+            LinkedList<int> list = new LinkedList<int>();
+            
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddLast(4);
+            foreach (int str in list)
+            {
+                Debug.WriteLine(str);
+            }
+            */
+
+        }
+    }
+}
---
+++ BagliListeler/BagliListeler/LinkedListADT.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagliListeler
+{
+    public class LinkedListADT
+    {
+        public Node Head;
+        public int Size = 0;
+        public void InsertFirst(int value)
+        {
+            Node tmpHead = new Node { Data = value };
+            if (Head == null)
+                Head = tmpHead;
+            else
+            { 
+                //En kritik nokta: tmpHead'in next'i //eski Head'i göstermeli
+                tmpHead.Next = Head;
+                //Yeni Head artık tmpHead oldu
+                Head = tmpHead;
+            }
+
+            //Bağlı listedeki eleman sayısı bir arttı
+            Size++;
+        }
+
+        public void InsertLast(int value)
+        {
+            Node newLast = new Node { Data = value };
+
+            if (Head == null)
+                Head = newLast;
+            else
+            {
+                Node oldLast = Head;
+                while (oldLast != null)
+                {
+                    if (oldLast.Next != null)
+                        oldLast = oldLast.Next;
+                    else
+                        break;
+                }
+                oldLast.Next = newLast;
+            }
+            Size++;
+        }
+        public void InsertPos(int position, int value)
+        {
+            if (position >= Size)
+                return;
+
+            Node newNode = new Node { Data = value };
+
+            if (Head == null)
+                Head = newNode;
+            else
+            {
+                Node posNode = Head;
+                int i  = 0;
+                while (posNode != null)
+                {
+                    if (i == position)
+                        break;
+
+                    if (posNode.Next != null)
+                    {
+                        posNode = posNode.Next;
+                        i++;
+                    }
+                }
+
+                Node tmpNext = posNode.Next;
+                posNode.Next = newNode;
+                newNode.Next = tmpNext;
+            }
+            Size++;
+        }
+        public void DeleteFirst()
+        {
+            if (Head == null)
+                return;
+
+            Node tmpHeadNext = Head.Next;
+            
+            if (tmpHeadNext == null)
+            {
+                Head = null;
+            }
+            else
+            {
+                Head = tmpHeadNext;
+            }
+
+            Size--;
+        }
+        public void DeleteLast()
+        {
+            Node lastPrevNode = new Node();
+            Node lastNode = Head;
+
+            while (lastNode != null)
+            {
+                if (lastNode.Next != null)
+                {
+                    lastPrevNode = lastNode;
+                    lastNode = lastNode.Next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            lastPrevNode.Next = null;
+            lastNode = null;
+            Size--;
+        }
+        public void DeletePos(int position)
+        {
+            if (position >= Size)
+                return;
+
+            Node posNode = Head;
+            Node lastPrevNode = new Node();
+            int i = 0;
+
+            while (posNode != null)
+            {
+                if (i == position)
+                    break;
+
+                if (posNode.Next != null)
+                {
+                    lastPrevNode = posNode;
+                    posNode = posNode.Next;
+                    i++;
+                }
+
+
+            }
+            lastPrevNode.Next = posNode.Next;
+            posNode = null;
+            Size--;
+        }
+
+        public  Node GetElement(int position)
+        {
+            Node posNode = Head;
+
+            if (position >= Size)
+                return null;
+
+            int i = 0;
+            while (posNode != null)
+            {
+                if (i == position)
+                    break;
+
+                if (posNode.Next != null)
+                {
+                    posNode = posNode.Next;
+                    i++;
+                }
+            }
+            return posNode;
+        }
+        public string DisplayElements()
+        {
+            if (Head == null)
+                return "Empty List";
+
+            string values = "";
+            Node tmpNode = Head;
+
+            while (tmpNode != null)
+            {
+                if (tmpNode.Next != null)
+                {
+                    values += tmpNode.Data.ToString() + "->";
+                    tmpNode = tmpNode.Next;
+                }
+                else
+                {
+                    values += tmpNode.Data.ToString() + "->";
+                    break;
+                }
+            }
+            return values.Substring(0, values.Length - 2);
+        }
+    }
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+    }
+}
